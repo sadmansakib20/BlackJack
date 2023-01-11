3b0e4511422e37206d52a60615ab206e72e86a60
sadmansakib20 <122421797+sadmansakib20@users.noreply.github.com>
2023-01-10 23:23:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,19 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
+class clear:
+ def __call__(self):
+  import os
+  if os.name==('ce','nt','dos'): os.system('cls')
+  elif os.name=='posix': os.system('clear')
+  else: print('\n'*120)
+ def __neg__(self): self()
+ def __repr__(self):
+  self();return ''
+
+clear=clear()
+
 import random
-from replit import clear 
 from art import logo 
 def deal_card():
   """Return a random card from the deck"""
